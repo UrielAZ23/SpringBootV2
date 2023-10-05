9c778c6fb815ca045a822285ae1ded37ec50aa70
Uriel Alvarez <uriel.az2303@gmail.com>
2023-10-05 12:28:03 -0600

    se sube la version 2 de spring con gradle

---
+++ src/test/java/com/platzi/market99/Market99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.market99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Market99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
